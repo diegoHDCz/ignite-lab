53c1abfcf21e07152dc2108ed1b1fa6da60ad140
Diego Czajka <diego.czajka@gmail.com>
2022-06-18 16:05:15 -0300

    Estrutura base dos projetos

---
+++ purchases/src/http/auth/test.resolver.ts
@@ -0,0 +1,14 @@
+import { UseGuards } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+import { PrismaService } from '../../database/prisma/prisma.service';
+import { AuthorizationGuard } from './authorization.guard';
+
+@Resolver()
+export class TestResolver {
+  constructor(private prisma: PrismaService) {}
+  @Query(() => String)
+  @UseGuards(AuthorizationGuard)
+  hello() {
+    return 'hello world!';
+  }
+}
